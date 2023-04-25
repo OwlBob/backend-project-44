5757c2e475d824f9ae0a7e08b9f677fb4170a1a2
OwlBob <tarakankist@yandex.ru>
2023-04-25 12:52:23 +0300

    fix bin/brain-games.js & src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
+
 const sayHello = () => {
-    console.log('Welcome to the Brain Games!');
-    let userName;
-    userName = readlineSync.question('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
+  console.log('Welcome to the Brain Games!');
+  let userName;
+  userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
 export default sayHello;
