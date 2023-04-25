62e02db19f37b5aaee8d57b50cb58b3dcbaec0af
OwlBob <tarakankist@yandex.ru>
2023-04-25 12:49:24 +0300

    add src/cli.js

---
+++ src/cli.js
@@ -0,0 +1,9 @@
+import readlineSync from 'readline-sync';
+const sayHello = () => {
+    console.log('Welcome to the Brain Games!');
+    let userName;
+    userName = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${userName}!`);
+};
+
+export default sayHello;
