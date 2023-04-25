5c753db486e31637e601b8fe66c9809785ea6cc1
OwlBob <tarakankist@yandex.ru>
2023-04-25 17:24:16 +0300

    fix via eslint src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,8 +2,7 @@ import readlineSync from 'readline-sync';
 
 const sayHello = () => {
   console.log('Welcome to the Brain Games!');
-  let userName;
-  userName = readlineSync.question('May I have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
