bee8f4f32b8ed6e46afc477889a6ea9df3c54ec0
OwlBob <tarakankist@yandex.ru>
2023-05-02 22:08:03 +0300

    add bin/brain-gcd.js & src/games/gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { cons } from '@hexlet/pairs';
+import generateRandomNum from '../tools.js';
+import brainGames from '../index.js';
+
+const gameRules = 'Find the greatest common divisor of given numbers.';
+
+const getNod = (firstNum, secondNum) => {
+  if (secondNum === 0) {
+    return firstNum;
+  }
+  return getNod(secondNum, firstNum % secondNum);
+};
+
+const questionAnswer = () => {
+  const firstNum = generateRandomNum(0, 100);
+  const secondNum = generateRandomNum(0, 100);
+  const generateRoundQuestion = `${firstNum} ${secondNum}`;
+
+  const rightAnswer = getNod(firstNum, secondNum);
+
+  return cons(generateRoundQuestion, rightAnswer);
+};
+
+export default () => brainGames(gameRules, questionAnswer);
