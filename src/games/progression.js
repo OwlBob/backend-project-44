816c596f55d77cd68f77233695104a76fb40a3db
OwlBob <tarakankist@yandex.ru>
2023-05-03 16:15:35 +0300

    add bin/brain-progression.js & src/games/progression.js

---
+++ src/games/progression.js
@@ -0,0 +1,31 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { cons } from '@hexlet/pairs';
+import generateRandomNum from '../tools.js';
+import brainGames from '../index.js';
+
+const gameRules = 'What number is missing in the progression?';
+
+const makeStepsProgression = (firstNum, step) => {
+  const collNum = [];
+  for (let i = 0; i < 10; i += 1) {
+    collNum.push(firstNum + i * step);
+  }
+  return collNum;
+};
+
+const questionAnswer = () => {
+  const firstNum = generateRandomNum(0, 15);
+  const step = generateRandomNum(1, 5);
+  const fullProgression = makeStepsProgression(firstNum, step);
+  const hiddenNum = generateRandomNum(0, fullProgression.length - 1);
+
+  const rightAnswer = fullProgression[hiddenNum];
+
+  fullProgression[hiddenNum] = '..';
+
+  const generateRoundQuestion = fullProgression.join(' ');
+
+  return cons(generateRoundQuestion, rightAnswer);
+};
+
+export default () => brainGames(gameRules, questionAnswer);
