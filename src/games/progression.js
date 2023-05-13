1e8aaa6367401e4fd6e6058d669b9b0f512fe60a
OwlBob <tarakankist@yandex.ru>
2023-05-13 20:31:18 +0300

    update package.json & fix src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -6,20 +6,21 @@ const gameRules = 'What number is missing in the progression?';
 
 const lengthProgression = 10;
 
-const makeProgression = (firstNum, step) => {
+const makeProgression = () => {
+  const firstNum = generateRandomNum(0, 15);
+  const step = generateRandomNum(1, 5);
   const collNum = [];
   for (let i = 0; i < lengthProgression; i += 1) {
     collNum.push(firstNum + i * step);
   }
+
   return collNum;
 };
 
 const questionAnswer = () => {
-  const firstNum = generateRandomNum(0, 15);
-  const step = generateRandomNum(1, 5);
-  const fullProgression = makeProgression(firstNum, step);
-
+  const fullProgression = makeProgression();
   const hiddenNum = generateRandomNum(0, fullProgression.length - 1);
+
   const rightAnswer = String(fullProgression[hiddenNum]);
 
   fullProgression[hiddenNum] = '..';
